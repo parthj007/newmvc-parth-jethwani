4310d90ebd837622b369faba3e9294fe77fe4dba
parthj007 <parthj007@users.noreply.github.com>
2023-04-26 19:04:16 +0530

    pagination in product

--- Model/Core/Pager.php
+++ Model/Core/Pager.php
@@ -1,9 +1,9 @@
 <?php
 class Model_Core_Pager 
 {
-    public $totalRecords = 0;
-    public $currentPage = 10;
-    public $numberOfPages = 0;
+    public $totalRecords = 0; //getter setter
+    public $currentPage = 10; //if page is 0 less than 1 then it will be 0 //getter setter
+    public $numberOfPages = 0; //calculate and getter for it 
     public $start = 1;
     public $previous = 0;
     public $next = 0;
@@ -14,16 +14,17 @@ class Model_Core_Pager
     {
         $this->totalRecords = $totalRecords;
         $this->currentPage = $totalRecords == 0 ? 1 : $currentPage;
-        $this->currentPage = $currentPage == 0 ? 1 : $currentPage;
+        $this->currentPage = $currentPage == 0 ? 1 : $currentPage; // getter setter
         $this->calulate();
         return $this;
     }
     //calculate start
     public function calulate()
     {
-        $this->start = $this->recordPerPage*($this->currentPage-1);
 
         $this->numberOfPages = ceil($this->totalRecords/$this->recordPerPage);
+
+        $this->start = $this->recordPerPage*($this->currentPage-1);
 
         $this->next = $this->currentPage ==  $this->numberOfPages ? $this->currentPage = $this->numberOfPages : $this->currentPage+1;
 
